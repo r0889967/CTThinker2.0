c945296113f7411d4167a8ab8ad4b71fbeb8d9a1
r0889967 <91953363+r0889967@users.noreply.github.com>
2025-10-14 18:28:27 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        MyFrame frame = new MyFrame();
-    }
-}
